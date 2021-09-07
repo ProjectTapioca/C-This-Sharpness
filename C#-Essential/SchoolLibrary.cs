99ed7eeff35890217f1704fac817b9629e08443a
Luke Ugie <39897299+ProjectLuke@users.noreply.github.com>
2021-09-07 16:05:59 -0700

    Adding SchoolLibrary.cs
    This file is the start of actually creating a class and putting all the tools learned in the C# linkedin learning video together.

---
+++ C#-Essential/SchoolLibrary.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SchoolLibrary
+{
+	class School
+	{
+		string SchoolName;
+		string SchoolAddress;
+		string SchoolCity;
+		string SchooleState;
+		string SchoolZip;
+		string PhoneNumber;
+		
+	}
+}
