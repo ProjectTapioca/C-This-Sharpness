c35fc1053a3028a7b092151eba8ba5f161dd7eac
Luke Ugie <39897299+ProjectLuke@users.noreply.github.com>
2021-09-07 14:58:04 -0700

    Adding Games.cs from replit
    This is a template off of a youtube video to allow using different files and classes from one file and put in the command to run it in the main.cs file

---
+++ C#-Essential/Games.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+	class Games
+	{
+		public void Run()
+		{
+			Console.WriteLine("Variable means blah");
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+		}
+	}
