bf744b9c1be00f01db5631edc903f9426b678fb1
Luke Ugie <39897299+ProjectLuke@users.noreply.github.com>
2021-09-16 11:38:38 -0700

    Create and Add EnglishPaper file
    Originally created in REPLIT for the C# Essentials

---
+++ C#-Essential/EnglishPaper.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolLibrary
+{
+	public class EnglishPaper
+	{
+		public string Title { get; set; }
+		public int MinimumWordCount { get; set; }
+		public string PaperText { get; set; }
+	}
+}
