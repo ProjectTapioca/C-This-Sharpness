e4fa90f07a6386d85836f3de6a0b59fc3afbb5f0
Luke Ugie <39897299+ProjectLuke@users.noreply.github.com>
2021-09-20 11:46:10 -0700

    Update Loops
    Adding updates of newly learned items and mainly syntax.

--- C#-Part2/Loops.cs
+++ C#-Part2/Loops.cs
@@ -2,7 +2,43 @@ namespace EssentialTraining
 {
     class ProgrammingLoops
     {
-        public in WhileLoop
+        public string BreakAndContinue()
+        {
+          var sb = new Stringbuilder();
+          var words = new List<string>() {"Bread", "Milk", "Eggs", "Cheese", "Apples"};
+          foreach(var word in words)
+          {
+            //This is saying that if a word in words start with the letter M, then skip everything underneath and go back around the loop one more time
+            if(word.Startswith("M")) continue;
+            
+            if (word.Startswith("C")) break;
+            
+            sb.AppendLine(word);
+          }
+          return sb.ToString();
+        }
+        public int DoWhileLoop()
+        {
+          var sum = 0;
+          var counter = 0;
+          do
+          {
+            sum += counter;
+            counter++;
+          }while (counter < 100);
+          return sum;
+        }
+        public int WhileLoop()
+        {
+          var sum = 0;
+          var counter = 0;
+          while (counter < 100)
+          {
+            sum += counter;
+            counter++;
+          }
+          return sum;
+        }
         public int ForEachLoop()
         {
             var numbers = new List<int>() { 1, 3, 5, 7, 9 };
